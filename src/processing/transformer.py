cb96ca1c3ed316cb5018981a229e8cb2734bdcb0
utkarsh.gupta <utkarsh.gupta@iongroup.com>
2026-01-24 19:10:27 +0530

    Added_user_and_time_features

--- src/processing/transformer.py
+++ src/processing/transformer.py
@@ -1,12 +1,16 @@
 import pandas as pd
+from src.domain.transaction import Transaction
 
 class TransactionTransformer:
     @staticmethod
-    def to_dataframe(transactions):
+    def to_dataframe(transactions: list[Transaction]):
         return pd.DataFrame(
             [{
                 "transaction_id": t.transaction_id,
+                "user_id": t.user_id,
                 "amount": t.amount,
-                "currency": t.currency
+                "currency": t.currency,
+                "date": t.date,
+                "time": t.time
             } for t in transactions]
         )
--- src/processing/validator.py
+++ src/processing/validator.py
@@ -2,7 +2,7 @@ class TransactionValidator:
     @staticmethod
     def validate(transactions):
         for transaction in transactions:
-            if transaction.transaction_id is None or transaction.amount is None:
+            if transaction.transaction_id is None or transaction.amount is None or transaction.currency is None or transaction.user_id is None:
                 raise ValueError(
                     f"Mandatory attribute missing for transaction: {transaction}"
                 )
