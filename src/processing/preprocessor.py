76e27fe66f096cd4dd842eeb8f2b1947a605d70b
utkarsh.gupta <utkarsh.gupta@iongroup.com>
2026-01-24 19:13:14 +0530

    Amount_features_added

--- src/processing/preprocessor.py
+++ src/processing/preprocessor.py
@@ -1,11 +1,14 @@
 from sklearn.preprocessing import StandardScaler
+import pandas as pd
+from amountFeatures import AmountFeatures
 
 class TransactionPreprocessor:
     def __init__(self):
         self.scaler = StandardScaler()
 
-    def normalize_amount(self, df):
-        df["amount"] = self.scaler.fit_transform(
-            df["amount"].values.reshape(-1, 1)
-        )
+    def get_amount_features(self, df: pd.DataFrame):
+        amount_features = AmountFeatures(self.scaler)
+        df = amount_features.normalize_amount(df)
+        df = amount_features.add_raw_and_normalized_amount_features(df)
+        df = amount_features.get_user_relative_features(df)
         return df
