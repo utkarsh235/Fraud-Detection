61f5eb9963f4b54eb51e2ffd7fc92d8109a544db
utkarsh.gupta <utkarsh.gupta@iongroup.com>
2026-01-26 12:35:02 +0530

    Add_features_extracted_to_processor

--- src/processing/preprocessor.py
+++ src/processing/preprocessor.py
@@ -1,6 +1,9 @@
 from sklearn.preprocessing import StandardScaler
 import pandas as pd
 from amountFeatures import AmountFeatures
+from currencyFeatures import CurrencyFeatures
+from datetimeFeatures import DatetimeFeatures
+from velocityFeatures import VelocityFeatures
 
 class TransactionPreprocessor:
     def __init__(self):
@@ -12,3 +15,22 @@ class TransactionPreprocessor:
         df = amount_features.add_raw_and_normalized_amount_features(df)
         df = amount_features.get_user_relative_features(df)
         return df
+
+    def get_currency_features(self, df: pd.DataFrame):
+        currency_features = CurrencyFeatures()
+        df = currency_features.get_currency_features(df)
+        return df
+    
+    def get_datetime_features(self, df: pd.DataFrame):
+        datetime_features = DatetimeFeatures()
+        df = datetime_features.get_datetime_features(df)
+        return df
+    
+    def get_velocity_features(self, df: pd.DataFrame):
+        velocity_features = VelocityFeatures()
+        df = velocity_features.get_velocity_features(df)
+        return df
+
+
+
+
